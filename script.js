62bf389cdb9b8131e6f0827db15564f507e8a6d0
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-28 14:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const user=document.getElementById("username");
+const psswrd=document.getElementById("password");
+const check=document.getElementById("checkbox");
+const sbtbtn=document.getElementById("submit");
+const existbtn=document.getElementById("existing");
+function load(){
+	if(localStorage.getItem("username")){
+		existbtn.style.display="block";
+	}
+}
+load();
+existbtn.addEventListener("click",function(e){
+	e.preventDefault();
+	alert(`Logged in as ${localStorage.getItem("username")}`);
+});
+sbtbtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	alert(`Logged in as ${user.value}`);
+	if(check.checked){
+		localStorage.setItem("username",user.value);
+		localStorage.setItem("password",psswrd.value);
+	}
+	else{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");	
+	}
+	load();
+});
